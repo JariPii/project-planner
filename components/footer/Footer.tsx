cc408649a246a3ab79f7a33ad80095be94a2ef34
Jari Leminaho <89419494+JariPii@users.noreply.github.com>
2025-05-07 17:13:23 +0200

    Adds footer and navbar components starters

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className='h-[15svh] flex items-center justify-center  bg-gray-300 '>
+      <h1 className='text-5xl'>Navbar</h1>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className='h-[15svh] w-full bg-gray-300 flex items-center justify-center'>
+      <h1 className='text-5xl'>Navbar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
